88126a9bd3d371e800770b9ad0dcad2c00ef7a3a
ds54e <17592097+ds54e@users.noreply.github.com>
2025-11-22 20:04:22 +0900

    Update ports_not_left_unused good fixture

--- fixtures/rules/ports_not_left_unused/good.sv
+++ fixtures/rules/ports_not_left_unused/good.sv
@@ -2,8 +2,11 @@ module m (
   input logic a,
   input logic b1, // reserved
   input logic b2, // used
-  output logic c
+  output logic c,
+  output logic d1,
+  output logic d2
 );
   assign c = a;
+  my_module inst (.d1, d2);
 endmodule
 
